b96f94c0c7ff90c6189d9a69d1cfcf3f99eb9ef3
l4x399 <83274160+l4x399@users.noreply.github.com>
2023-04-22 12:57:16 +0800

    Add bootstrap

--- npm install.md
+++ npm install.md
@@ -4,6 +4,7 @@ npm install             \
 react-scripts@4.0.3     \
 react-router-dom@5.3.4  \
 axios                   \
+bootstrap@4.6.2         \
 bcrypt                  \
 cors                    \
 express                 \
--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './styles/index.css';
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/js/bootstrap.js';
+import './css/index.css'
+
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
