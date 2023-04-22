a42f622096ba7e4e6099c95a60c77d82e0ac751e
l4x399 <83274160+l4x399@users.noreply.github.com>
2023-04-22 22:14:36 +0800

    navbar wiop

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,30 +1,36 @@
 const NavBar = ({ isLoggedIn, handleLogout }) => {
     return (
-        <nav>
-            <ul>
-                <li>
-                <a href="/">Home</a>
+        <nav class="navbar navbar-expand-lg bg-light navbar-light">
+            <a class="navbar-brand" href="/">Food App</a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="collapsibleNavbar">
+            <ul class="navbar-nav">
+                <li class="nav-item">
+                <a class="nav-link" href="/">Home</a>
                 </li>
                 {isLoggedIn ? (
-                <li>
-                    <a href="/subpage">Subpage</a>
+                <li class="nav-item">
+                    <a class="nav-link" href="/subpage">Subpage</a>
                 </li>
                 ) : null}
                 {isLoggedIn ? (
-                <li>
-                    <button onClick={handleLogout}>Logout</button>
+                <li class="nav-item">
+                    <button class="nav-link" onClick={handleLogout}>Logout</button>
                 </li>
                 ) : (
                 <>
-                    <li>
-                    <a href="/login">Login</a>
+                    <li class="nav-item">
+                    <a class="nav-link" href="/login">Login</a>
                     </li>
-                    <li>
-                    <a href="/register">Register</a>
+                    <li class="nav-item">
+                    <a class="nav-link" href="/register">Register</a>
                     </li>
                 </>
                 )}
             </ul>
+            </div>
         </nav>
     );
 };
